200c48d0b945d8388dfdfd0dc5c7114b80fea4ed
Jahid Islam Rafsan <67214300+prorafsan@users.noreply.github.com>
2020-12-02 07:31:17 -0800

    Add files via upload

---
+++ javaScript/JavaScript.js
@@ -0,0 +1,22 @@
+ let manu_btn = function () {
+
+     let manu = document.querySelector(".manu");
+
+
+     if (manu.style.display == "block") {
+         manu.style.display = "none";
+     } else {
+         manu.style.display = "block";
+     }
+};
+let aboutContent = function () {
+
+    let secendAbout = document.querySelector(".secendAbout");
+
+
+    if (secendAbout.style.display == "block") {
+        secendAbout.style.display = "none";
+    } else {
+        secendAbout.style.display = "block";
+    }
+};
